87b689e6fb409e1871771ee5ce95ccde5f7c6128
Victor Biga <victor.biga@gmail.com>
2019-08-09 17:00:12 +0100

    adding date to the commit

--- cmdb.sh
+++ cmdb.sh
@@ -5,5 +5,5 @@ ansible -m setup --tree /tmp/out/ all
 ansible-cmdb -t markdown /tmp/out/ > overview.md
 ansible-cmdb -t markdown_split /tmp/out
 git add -A
-git commit -m "Update inventory"
+git commit -m "Update inventory `date`"
 git push
