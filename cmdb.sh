4c6b27956c9453a806759f2c4a5039f9a1583f6c
Andreas Krivas <andreas131313@gmail.com>
2019-08-09 15:28:32 +0000

    Update inventory

--- cmdb.sh
+++ cmdb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git pull
 ansible -m setup --tree /tmp/out/ all
 ansible-cmdb -t markdown /tmp/out/ > overview.md
 ansible-cmdb -t markdown_split /tmp/out
+git add -A
+git commit -m "Update inventory"
+git push
